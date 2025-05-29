39548459f2f01adc9767c3d4452fd1ba67632b9f
Lucas Mendes <lucas.mendes.1234@hotmail.com>
2025-05-29 19:26:29 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var formulario = document.getElementById("form-cadastro")
+
+formulario.addEventListener("submit", function(event){
+    event.preventDefault()
+    var campoNome = document.getElementById("nome").value
+    var campoEmail = document.getElementById("email").value
+
+    console.log(campoEmail, campoNome)
+})
